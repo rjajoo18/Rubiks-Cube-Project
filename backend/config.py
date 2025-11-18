00309bb8b664347fbdb860526ab92e80780e80c8
rjajoo18 <rishjajoo18@gmail.com>
2025-11-18 16:54:42 -0500

    Stop tracking config.py

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Rj_AIME2024!@db.tyvciqxqgkkxnersccka.supabase.co:5432/postgres"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "super-secret-key-change-this"
